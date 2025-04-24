0b82434c43922351d862802eaa218717b2f9fc36
theoblin <blintheo23@gmail.com>
2025-04-24 12:11:29 +0200

    adding services and config

---
+++ BonjourBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace theoblin\BonjourBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KjonskiHowToBundle extends Bundle
+{
+
+}
---
+++ Service/BonjourService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Theoblin\BonjourBundle\Service;
+
+class BonjourService
+{
+    public function sayHello(): string
+    {
+        return 'Bonjour depuis le bundle ! 👋';
+    }
+}
