1201833204760c8eaaa112d3a973bd5728ed7e3a
theoblin <blintheo23@gmail.com>
2025-04-24 14:56:20 +0200

    adding code

--- BonjourBundle.php
+++ BonjourBundle.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace theoblin\BonjourBundle;
-
+namespace Theoblin\BonjourBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
-class KjonskiHowToBundle extends Bundle
+class BonjourBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
 
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
 }
